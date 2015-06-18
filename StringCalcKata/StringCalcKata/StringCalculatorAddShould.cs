ca0b5b31fa960d26336ca8d891a014dbcd7d65f2
Steve Smith <steve@kentsmiths.com>
2015-06-18 14:29:19 -0400

    Add support for empty string.

--- StringCalcKata/StringCalcKata/StringCalculatorAddShould.cs
+++ StringCalcKata/StringCalcKata/StringCalculatorAddShould.cs
@@ -5,6 +5,13 @@ namespace StringCalcKata
     [TestFixture]
     public class StringCalculatorAddShould
     {
-        
+        [Test]
+        public void ReturnZeroGivenEmptyString()
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add("");
+
+            Assert.AreEqual(0, result);
+        }
     }
 }
